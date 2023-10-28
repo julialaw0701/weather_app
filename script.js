257d5e04ed221f65aec6bbb3468e827cb83726c8
Julia Law <julialaw0701@gmail.com>
2023-10-28 18:48:01 +0800

    updated into index.html

---
+++ script.js
@@ -0,0 +1,6 @@
+let myImage=document.querySelector("./sunny.jpg")
+myimage.src="sunny.jpg"
+
+if (weather.toLowerCase () === "sunny")(my image src="https://www.google.com")
+else if (weather.tolowerCase () === "sunny")(my image src=)
+alert ('sorry, we do not have this kind of weather.'
